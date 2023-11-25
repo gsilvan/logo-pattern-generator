9d3e56aa3679e4f04547e01caa0d8affcab05765
Silvan Gümüsdere <silvan@trollbox.org>
2023-11-25 14:39:05 +0100

    first prototype

---
+++ src/App.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import "./App.css";
+import Canvas from "./Canvas";
+
+function App() {
+  const [scale, setScale] = useState(100);
+  const [rotation, setRotation] = useState(0);
+  const [xlogoPadding, setXLogoPadding] = useState(2);
+  const [ylogoPadding, setYLogoPadding] = useState(2);
+  const [image, setImage] = useState("");
+  const [width, setWidth] = useState(25);
+  const [height, setHeight] = useState(25);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        setImage(event.target?.result as string);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const settings = [
+    {
+      name: "scale",
+      description: "Logogröße",
+      value: scale,
+      setValue: setScale,
+      min: 0,
+      max: 100,
+      unit: "%",
+    },
+    {
+      name: "rotation",
+      description: "Drehung",
+      value: rotation,
+      setValue: setRotation,
+      min: -180,
+      max: 180,
+      unit: "°",
+    },
+    {
+      name: "xLogoPadding",
+      description: "X Padding",
+      value: xlogoPadding,
+      setValue: setXLogoPadding,
+      min: 0,
+      max: 20,
+      unit: "cm",
+    },
+    {
+      name: "yLogoPadding",
+      description: "Y Padding",
+      value: ylogoPadding,
+      setValue: setYLogoPadding,
+      min: 0,
+      max: 20,
+      unit: "cm",
+    },
+    {
+      name: "width",
+      description: "Breite",
+      value: width,
+      setValue: setWidth,
+      min: 18,
+      max: 35,
+      unit: "cm",
+    },
+    {
+      name: "height",
+      description: "Länge",
+      value: height,
+      setValue: setHeight,
+      min: 18,
+      max: 35,
+      unit: "cm",
+    },
+  ];
+
+  return (
+    <div className="flex w-full h-full">
+      <div className="w-2/5 h-full">
+        {settings.map((setting) => (
+          <div className="flex">
+            <div className="w-32">
+              <label htmlFor={setting.name}>{setting.description}</label>
+            </div>
+
+            <input
+              className="w-64"
+              value={setting.value}
+              onChange={(e) => setting.setValue(parseInt(e.target.value))}
+              type="range"
+              id={setting.name}
+              name={setting.name}
+              min={setting.min}
+              max={setting.max}
+            />
+            <div className="px-2.5">
+              {setting.value} {setting.unit}
+            </div>
+          </div>
+        ))}
+        <div>
+          <input type="file" onChange={handleImageUpload} accept="image/*" />
+        </div>
+      </div>
+      <div className="w-3/5">
+        <div className="w-full">
+          <Canvas
+            image={image}
+            width={width}
+            height={height}
+            rotation={rotation}
+            scale={scale}
+            backgroundColor={"#0803AF"}
+            xGap={xlogoPadding}
+            yGap={ylogoPadding}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Canvas.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { useRef, useEffect, useMemo } from "react";
+import { log } from "util";
+
+function cmToPixel(cm: number): number {
+  return Math.round(28 * cm);
+}
+
+export default function Canvas({
+  width,
+  height,
+  image,
+  rotation,
+  scale,
+  backgroundColor,
+  xGap,
+  yGap,
+}: {
+  width: number;
+  height: number;
+  image: string;
+  rotation: number;
+  scale: number;
+  backgroundColor: string;
+  xGap: number;
+  yGap: number;
+}) {
+  const canvasSize = {
+    width: 1100,
+    height: 1100,
+  };
+  const pixelWidth = cmToPixel(width);
+  const pixelHeight = cmToPixel(height);
+  const canvasRef = useRef(null);
+
+  const logo = new Image();
+  logo.src = image;
+
+  const base64Image = "";
+
+  const downloadCanvas = () => {
+    const canvas = document.getElementById("mycanvas") as HTMLCanvasElement;
+    if (canvas) {
+      const dataUrl = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = "canvas_image.png";
+      link.click();
+    }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    // @ts-ignore
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, 1200, 1200);
+
+    ctx.save(); // Save the current state
+    ctx.translate(canvasSize.width / 2, canvasSize.height / 2); // Move the origin to the center
+    ctx.rotate((rotation * Math.PI) / 180);
+    for (let i = -10; i < 10; i++) {
+      for (let j = -10; j < 10; j++) {
+        ctx.drawImage(
+          logo,
+          i * logo.width + i * cmToPixel(xGap),
+          j * logo.height + j * cmToPixel(yGap),
+          (logo.width * scale) / 100,
+          (logo.height * scale) / 100,
+        );
+      }
+    }
+    ctx.restore();
+
+    ctx.lineWidth = 20;
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(20, 20, pixelWidth, pixelHeight);
+
+    ctx.fillStyle = "#000";
+
+    ctx.font = "24px Mono";
+    ctx.fillStyle = "#fff";
+    ctx.fillText(
+      `${width} cm x ${height} cm`,
+      pixelWidth - 250,
+      pixelHeight + 28,
+    );
+  }, [
+    width,
+    height,
+    image,
+    rotation,
+    scale,
+    backgroundColor,
+    logo,
+    pixelHeight,
+    pixelWidth,
+    canvasSize.width,
+    canvasSize.height,
+    xGap,
+    yGap,
+  ]);
+
+  return (
+    <>
+      <canvas
+        id="mycanvas"
+        ref={canvasRef}
+        width={canvasSize.width}
+        height={canvasSize.height}
+      />
+      <button onClick={downloadCanvas}>Download PNG</button>
+    </>
+  );
+}
