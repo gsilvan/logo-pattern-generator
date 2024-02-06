fe5a7748a1c15ab7ff8e6e7772f3227df3b8d0cb
silvan <silvan@trollbox.info>
2024-02-06 09:48:35 +0100

    buttons: make buttons more like real buttons

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -123,10 +123,16 @@ export default function Canvas({
       />
       <div className="fixed right-14 bottom-5">
         <div className="flex gap-3">
-          <button className="font-bold" onClick={downloadCanvasAsPdf}>
+          <button
+            className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+            onClick={downloadCanvasAsPdf}
+          >
             Download PDF
           </button>
-          <button className="font-bold" onClick={downloadCanvas}>
+          <button
+            className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+            onClick={downloadCanvas}
+          >
             Download PNG
           </button>
         </div>
